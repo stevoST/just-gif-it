88a12cea91045a82e42ce9fc7a18ce9cc0c9e87e
Starosta Štefan <starosta@domain.local>
2019-03-14 09:37:43 +0100

    added curl command

--- src/main/java/schultz/dustin/io/justgifit/JustGifItApplication.java
+++ src/main/java/schultz/dustin/io/justgifit/JustGifItApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JustGifItApplication {
 
+
+	//curl -F file=test.mp4 -F start=0 -F end=0 -F speed=1 -F repeat=0 localhost:8080/upload
 	public static void main(String[] args) {
 		SpringApplication.run(JustGifItApplication.class, args);
 	}
